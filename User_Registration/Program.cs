19b159f5118af0036c3ba72c0f554f4521556cc3
KajalShendage <shendagekajal123@gmail.com>
2022-05-30 15:11:15 +0530

    Add project files.

---
+++ User_Registration/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using User_Registration;
+Console.WriteLine("Welcome to Program for User Registration");
+
+UserRegistration UserRegistration = new UserRegistration();
+
+Console.WriteLine(UserRegistration.ValidateFirstName("Kajal"));
+
+Console.WriteLine(UserRegistration.ValidateLastName("Shendage"));
+
+Console.WriteLine(UserRegistration.ValidateEmail("kajal123@gmail.com"));
+
+Console.WriteLine(UserRegistration.ValidatePhoneNumber("+91 9924156412"));
+
+Console.WriteLine(UserRegistration.ValidatePassword("Zxcvbnmas"));
+
+Console.WriteLine(UserRegistration.ValidatePassword2("Zmcnhdas")); 
+
+Console.WriteLine(UserRegistration.ValidatePassword3("1Zmxnchasd"));
+
+Console.WriteLine(UserRegistration.ValidatePassword4("Z1xxccxv@"));
+
+
---
+++ User_Registration/UserRegistration.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace User_Registration
+{
+    public class UserRegistration
+    {
+        public static string REGEX_FIRSTNAME = "[A-Z]{1}[A-Za-z]{2,}$";
+
+        public static string REGEX_LASTNAME = "[A-Z]{1}[A-Za-z]{2,}$";
+
+       public static string REGEX_EMAIL = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+
+        public static string REGEX_PHONENUMBER = "^[/+91]*[ ]*[/-]*[0-9]{10}$";
+
+        public static string REGEX_PASSWORD = "^[A-Za-z]{8,}$";
+
+        public static string REGEX_PASSWORD2 = "^[A-Z]+[A-Za-z]{7,}$";
+
+        public static string REGEX_PASSWORD3 = "^[A-Z0-9]+[A-Za-z0-9]{7,}$";
+
+        public static string REGEX_PASSWORD4 = "^[A-Z0-9]+[A-Za-z0-9]{7,}[&^%$#@!]$";
+
+        public bool ValidateFirstName(string firstname)
+        {
+            return Regex.IsMatch(firstname, REGEX_FIRSTNAME);
+
+        } 
+        public bool ValidateLastName(string lastname)
+        {
+            return Regex.IsMatch(lastname, REGEX_LASTNAME);
+        }
+       public bool ValidateEmail(string email)
+        {
+            return Regex.IsMatch(email, REGEX_EMAIL);
+        }
+        public bool ValidatePhoneNumber(string phonenumber)
+        {
+            return Regex.IsMatch(phonenumber, REGEX_PHONENUMBER);
+        }
+        public bool ValidatePassword(string password)
+        {
+            return Regex.IsMatch(password, REGEX_PASSWORD);
+        }
+        public bool ValidatePassword2(string password2)
+        {
+            return Regex.IsMatch(password2, REGEX_PASSWORD2);
+        }
+
+        public bool ValidatePassword3(string password3)
+        {
+            return Regex.IsMatch(password3, REGEX_PASSWORD3);
+        }
+        public bool ValidatePassword4(string password4)
+        {
+            return Regex.IsMatch(password4, REGEX_PASSWORD4);
+        }
+
+
+    }
+}
